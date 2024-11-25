da87985e1122383caaf141821cb880ba5ad41ced
MGilangSeftian <143242901+MGilangSeftian@users.noreply.github.com>
2024-11-25 10:43:19 +0700

    Halaman Detail Novel

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 
+import 'package:cobaprojekpab/Screens/tampilan_detailNovel.dart';
 import 'package:cobaprojekpab/Screens/tampilan_home.dart';
 import 'package:cobaprojekpab/Screens/tampilan_profile.dart';
+import 'package:cobaprojekpab/data_list_novel/data_novel.dart';
 
 import 'package:flutter/material.dart';
 
@@ -20,10 +22,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // // home: const MyHomePage(title: 'Flutter Demo Home Page'),
       home: TampilanHome(),
-      home: TampilanProfile(),
-
+      // home: TampilanProfile(),
     );
   }
 }
