763a03acaec4962e90c746f2a8f8b9014325e5ca
Wildan <shahabwildan1225@gmail.com>
2024-11-28 14:15:42 +0700

    tesssssssssssssssss

---
+++ lib/Screens/tampilan_cobacoba.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TampilanCobacoba extends StatefulWidget {
+  const TampilanCobacoba({super.key});
+
+  @override
+  State<TampilanCobacoba> createState() => _TampilanCobacobaState();
+}
+
+class _TampilanCobacobaState extends State<TampilanCobacoba> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
